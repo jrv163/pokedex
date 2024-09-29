1cc747378b46f6c42dff8e97f233f6e2dc096e45
Jamer Rodriguez <jrv163@hotmail.com>
2024-09-28 21:59:28 -0500

    CRUD Validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PokemonModule } from './pokemon/pokemon.module';
+import { CommonModule } from './common/common.module';
 
 
 @Module({
@@ -14,6 +15,8 @@ import { PokemonModule } from './pokemon/pokemon.module';
       MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
 
       PokemonModule,
+
+      CommonModule,
   ],
  
 })
--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -1,8 +1,8 @@
-import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
 import { Pokemon } from './entities/pokemon.entity';
-import { Model } from 'mongoose';
+import { isValidObjectId, Model, RootFilterQuery } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 
 
@@ -10,24 +10,21 @@ import { InjectModel } from '@nestjs/mongoose';
 export class PokemonService {
 
   constructor(
-    @InjectModel( Pokemon.name )
+    @InjectModel(Pokemon.name)
     private readonly pokemonModel: Model<Pokemon>
-  ) {}
+  ) { }
 
   async create(createPokemonDto: CreatePokemonDto) {
-   createPokemonDto.name = createPokemonDto.name.toLowerCase();
+    createPokemonDto.name = createPokemonDto.name.toLowerCase();
 
     try {
-      const pokemon = await this.pokemonModel.create( createPokemonDto );
+      const pokemon = await this.pokemonModel.create(createPokemonDto);
       return pokemon;
-      
-    } catch (error) {
-       if ( error.code === 11000 ) {
-         throw new BadRequestException(`Pokemon exists un db ${ JSON.stringify( error.keyValue ) }`);
-       }
 
-       console.log( error );
-        throw new InternalServerErrorException('Error creating pokemon - check server logs');
+    } catch (error) {
+      
+      this.handleExceptions( error )
+
     }
 
   }
@@ -36,15 +33,87 @@ export class PokemonService {
     return `This action returns all pokemon`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} pokemon`;
+
+
+  async findOne(term: string) {
+
+    let pokemon = null; // let pokemon va a ser de tipo entity o null si no encuentra el documento de mongoose
+
+    if (!isNaN(+term)) { // si term es un número
+      pokemon = await this.pokemonModel.findOne({ no: term })
+    }
+
+    // MONGO ID
+    if (!pokemon && isValidObjectId(term)) {
+      pokemon = await this.pokemonModel.findById(term);
+    }
+
+    // NAME
+    if (!pokemon) {
+      pokemon = await this.pokemonModel.findOne({ name: term.toLowerCase().trim() })
+    }
+
+
+    if (!pokemon)
+      throw new NotFoundException(`Pokemon with id, name or no ${term} not found`)
+
+
+    return pokemon;
   }
 
-  update(id: number, updatePokemonDto: UpdatePokemonDto) {
-    return `This action updates a #${id} pokemon`;
+
+
+  // para hacer la actualización, primero se debe consultar si ya existe el pokemon. Y para eso tenemos el findOune que hace todas esas validaciones
+  async update(term: string, updatePokemonDto: UpdatePokemonDto) {
+
+    const pokemon = await this.pokemonModel.findById(term);
+    if (updatePokemonDto.name)
+      updatePokemonDto.name = updatePokemonDto.name.toLowerCase();
+
+    try {
+
+      await pokemon.updateOne(updatePokemonDto)
+      return { ...pokemon.toJSON(), ...updatePokemonDto };
+
+    } catch (error) {
+      // console.log(error)
+
+      this.handleExceptions( error )
+
+    }
+
+
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} pokemon`;
+  async remove(id: string) {
+    
+    // const pokemon = await this.pokemonModel.findById( term ); // se deberia usar el findOne para usar las validaciones del s
+
+    // try {
+    //   await pokemon.deleteOne();
+    // } catch (error) {
+    //   throw new BadRequestException(`Pokemon with Id, name or no ${ term } does not exist in DB`)
+    // }
+    // return { term }
+    // const deletedPokemon = await this.pokemonModel.findByIdAndDelete( term );
+
+    const { deletedCount } = await this.pokemonModel.deleteOne({ _id: id });
+    if ( deletedCount === 0 )
+        throw new BadRequestException(`Pokemon with id ${ id } not found`)
+
+
+    return ;
+  }
+
+
+
+  private handleExceptions(error: any) {
+    if (error.code === 11000) {
+      throw new BadRequestException(`Pokemon exists un db ${JSON.stringify(error.keyValue)}`);
+    }
+
+    console.log(error);
+    throw new InternalServerErrorException('Error creating pokemon - check server logs');
+
   }
 }
